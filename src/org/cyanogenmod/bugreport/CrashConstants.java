4a57343a9ecf62c521b5e2895cde45290867add0
Adnan Begovic <adnan@cyngn.com>
2015-02-24 16:28:14 -0800

    CMBugReport: Allow bug report without attaching logs.
    Change-Id: Id45891444f2c211578fc00211fa552b7a5c346cb

---
+++ src/org/cyanogenmod/bugreport/CrashConstants.java
@@ -0,0 +1,11 @@
+package org.cyanogenmod.bugreport;
+
+/**
+ * Created by Adnan on 2/24/15.
+ */
+public final class CrashConstants {
+    public static final String SCREEN_SHOT_ATTACHMENT_PREF =
+            "org.cyanogenmod.bugreport.AddScreenshot";
+    public static final String BUG_REPORT_ATTACHMENT_PREF =
+            "org.cyanogenmod.bugreport.AttachBugReport";
+}
